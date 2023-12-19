910783f3522995926b648c111804d903acea55cf
Bartłomiej Kudzia <bar.kud.11340@gmail.com>
2023-12-19 11:05:10 +0100

    Link avizonet with cerealizer

--- src/avizonet.cpp
+++ src/avizonet.cpp
@@ -1,5 +1,7 @@
 #include <avizonet.h>
 
+#include <cerealizer.h>
+
 #include <iostream>
 
 namespace AvizoNet
@@ -7,7 +9,13 @@ namespace AvizoNet
 
 void Hello()
 {
-    std::puts("Hello, world!");
+    Cerealizer::Context ctx;
+    ctx.Serialize<std::string>("Hello, world!");
+    ctx.Rewind();
+
+    std::string message;
+    ctx.Deserialize(message);
+    std::cout << message << std::endl;
 }
 
 }
