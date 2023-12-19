56698cca997ec820cdc3d70d7c987cffb07f9a7b
Bartłomiej Kudzia <bar.kud.11340@gmail.com>
2023-12-19 10:57:44 +0100

    Add simple project structure

---
+++ include/avizonet.h
@@ -0,0 +1,11 @@
+#ifndef AVIZONET_H
+#define AVIZONET_H
+
+namespace AvizoNet
+{
+
+void Hello();
+
+}
+
+#endif
---
+++ src/avizonet.cpp
@@ -0,0 +1,13 @@
+#include <avizonet.h>
+
+#include <iostream>
+
+namespace AvizoNet
+{
+
+void Hello()
+{
+    std::puts("Hello, world!");
+}
+
+}
